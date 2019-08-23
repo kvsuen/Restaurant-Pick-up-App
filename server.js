49bca6b7100b0514ab550d617ed1c5e954eae743
Kevin Suen <kevinsuen11@gmail.com>
2019-08-23 18:44:22 -0400

    Add GET POST routes and create temporary data objs

--- server.js
+++ server.js
@@ -9,12 +9,47 @@ const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
+
+// TEMPORARY DATABASE
+const users = [
+  {
+    id: 0,
+    name: "test",
+    email: "test@test.com",
+    password: "test",
+    phone: 1231231234,
+    creditCardNumber: 1234123412341234,
+    cardExpiryDate: 1120,
+    cardSecurityCode: 123
+  }
+];
+
+const userOrder = [
+  {
+    menuItem: "fries",
+    quantity: 3,
+    purchasePrice: 500
+  },
+  {
+    menuItem: "krab",
+    quantity: 3,
+    purchasePrice: 50000
+  }
+];
+
+// Cookie session set up
+app.use(cookieSession({
+  name: "session",
+  keys: ["game"],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -48,6 +83,43 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/orders", (req, res) => {
+  res.render("orders");
+});
+
+app.get("/checkout", (req, res) => {
+  res.render("checkout");
+});
+
+app.post("/register", (req, res) => {
+  // TODO
+  // Do error checking (helper func)
+  // Check for empty form fields
+  // Call a function to make an INSERT query to database
+  res.redirect('/');
+});
+
+app.post("/login", (req, res) => {
+  // TODO
+  // Do error checking (helper func)
+  res.redirect('/');
+});
+
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect('/');
+});
+
+app.post("/checkout", (req, res) => {
+  // TO DO
+  // Call a function to query database
+  res.redirect('/');
 });
 
 app.listen(PORT, () => {
