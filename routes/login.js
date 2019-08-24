2e9be630b12ac50909de92698814e90aa1d14c6d
Kevin Suen <kevinsuen11@gmail.com>
2019-08-24 18:43:17 -0400

    Implement initial login feature

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,22 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    req.session.user_id = req.params.id;
-    res.redirect("/");
+    const {email, password} = req.body;
+    console.log(email, password);
+
+    db.userLogin(email, password)
+      .then(id => {
+        if (!id) {
+          res.send({error: "error"});
+          return;
+        } else {
+          console.log(id);
+          req.session.userId = id.id;
+          res.redirect("/");
+        }
+      }).catch(err => console.error(null, err.stack));
+
+
   });
 
   return router;
