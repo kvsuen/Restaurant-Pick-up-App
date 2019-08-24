53aced60b18ca9936d569f3921e83031246b55bc
Kevin <kevinyoo0203@yahoo.com>
2019-08-24 17:29:42 +0000

    more changes

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,13 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate users table
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  credit_card_number VARCHAR(255),
+  card_expiry_date VARCHAR(255),
+  card_security_code VARCHAR(255),
+  phone_number VARCHAR(255) NOT NULL
 );
