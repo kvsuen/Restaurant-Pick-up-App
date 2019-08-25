6fd6d2df4adafa3a79606554f1ed345f453e0f40
Kevin Suen <kevinsuen11@gmail.com>
2019-08-25 14:29:31 -0400

    Remove console logs

--- routes/login.js
+++ routes/login.js
@@ -4,21 +4,16 @@ const router  = express.Router();
 module.exports = (db) => {
   router.post("/", (req, res) => {
     const {email, password} = req.body;
-    console.log(email, password);
-
     db.userLogin(email, password)
       .then(id => {
         if (!id) {
           res.send({error: "error"});
           return;
         } else {
-          console.log(id);
           req.session.userId = id.id;
           res.redirect("/");
         }
       }).catch(err => console.error(null, err.stack));
-
-
   });
 
   return router;
