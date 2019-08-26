ce041abf7f37765ef714834993fc5250b621f615
cd <azzamathar@hotmail.com>
2019-08-26 19:41:14 +0000

    implement twilio for checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router  = express.Router();
 
+const twilio = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("checkout");
   });
 
   router.post("/", (req, res) => {
-    // TO DO
-    // Call a function to query database
-    res.redirect("/");
-  });
+  const numbers = [Number(process.env.TWILIO_NUMBER_SEND_1), Number(process.env.TWILIO_NUMBER_SEND_2)] ;
+  const body = ['Your order has been placed! It will be ready in 25-30 minutes.', 'An order has been placed by Kevin!'];
 
+  Promise.all(
+    numbers.map((number, index) => {
+      return twilio.messages.create({
+        to: number,
+        from: process.env.TWILIO_NUMBER,
+        body: body[index]
+      });
+    })
+  )
+  .then(messages => {
+      console.log('Messages sent!');
+      req.session = null;
+      res.redirect("/");
+    }).catch(err => console.error(err))
+
+  })
   return router;
 };
