f315fffae2331b1fd80eb88bc7706e5d4b184814
Kevin Suen <kevinsuen11@gmail.com>
2019-08-28 00:31:48 -0400

    Implement initial responsive cart

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-  const incrementItemLocalStorage = function ($input, quantity) {
-    // slice at 10 since id starts at index 10 from 'menu_item_id'
-    const id = Number($input.parent().find("input").attr('name').slice(10));
+  const generateItemObject = function($input, quantity) {
+    // slice at 20 since id starts at index 20 from 'menu-text menu_item_id'
+    const id = Number($input.parent().siblings().attr('class').slice(20));
     const name = $input.parent().siblings('.menu-text').find('.dish-name').text();
     const price = $input.parent().siblings('.price').text();
 
@@ -13,51 +13,133 @@ $(document).ready(function() {
       price,
     };
 
-    window.localStorage.setItem(id, JSON.stringify(item));
+    return item;
+  };
+
+  const incrementItemLocalStorage = function ($input, quantity) {
+    const item = generateItemObject($input, quantity);
+
+    window.localStorage.setItem(item.id, JSON.stringify(item));
   };
 
   const removeItemFromLocalStorage = function ($input) {
-    const id = Number($input.parent().find("input").attr('name').slice(10));
+    const id = Number($input.parent().siblings().attr('class').slice(20));
 
     window.localStorage.removeItem(id);
   };
 
+  const getNumOfItems = function() {
+    const numItems = Object.keys(window.localStorage).length;
+
+    return numItems;
+  };
+
+  const renderCartItem = function($input, quantity) {
+    const item = generateItemObject($input, quantity);
+
+    const markup = `
+    <p>${item.quantity}</p>
+    <h3 class="${item.id}">${item.name}</h3>
+    <p>$${Number(item.price * item.quantity).toFixed(2)}</p>
+    <span class="remove-item"><img src="/images/x-mark.png" alt="Remove from order" title="Remove from order"></span>
+    `;
+
+    $('#order-container').append($('<article>').addClass(`container ${item.id}`).html(markup));
+
+    // x mark remove from cart
+    $(".remove-item").on("click", function () {
+      const $button = $(this);
+
+      const id = Number($button.siblings("h3").attr("class"));
+      window.localStorage.removeItem(id);
+      $(`article.${id}`).remove();
+      renderCartTotal();
+    })
+  };
+
+  const removeItemFromCart = function($input) {
+    const id = Number($input.parent().siblings().attr('class').slice(20));
+
+    $(`article.${id}`).remove();
+  };
+
+  const getCartTotal = function() {
+    const numKeys = getNumOfItems();
+    let sum = 0.00;
+
+    for (let index = 0; index < numKeys; index++) {
+      let item = JSON.parse(window.localStorage.getItem(window.localStorage.key(index)));
+      sum += item.quantity * item.price;
+    }
+
+    return sum.toFixed(2);
+  };
+
+  const renderCartTotal = function() {
+    $('section.total div.container p').empty();
+    $('section.total div.container p').append(`$${getCartTotal()}`);
+  }
+
+  const renderCartItems = function($input) {
+    const numKeys = getNumOfItems();
+
+    for (let index = 0; index < numKeys; index++) {
+      renderCartItem();
+    }
+
+  };
+
+
+
   $(function () {
 
+    // console.log($("tr .add").siblings().attr('class'));
+
+    // #### NEED TO GIVE IT AN CLASS ID USING EJS #### //
     $("tr .add").prepend('<button type="button" class="plus increment">+</button>');
+    $("tr .add").append(`<input class="quantity" type="number" name="quantity" value="0" max="100" min="0" disabled></input>`)
     $("tr .add").append('<button type="button" class="minus increment">-</button>');
     $("tr .add").append('<span class="reset"><img src="/images/x-mark.png" alt="Remove from order" title="Remove from order"></span>');
 
+    // console.log($("tr .add").children("input").attr('value', 1));
+
     // Implement onclick for the + - buttons
     $(".increment").on("click", function () {
-      var $button = $(this);
-      var oldValue = $button.parent().find("input").val();
+      const $button = $(this);
+      const oldValue = $button.parent().find("input").val();
+      let newVal = oldValue;
+
+      const id = Number($button.parent().siblings().attr('class').slice(20));
+
 
       if ($button.text() === "+") {
-        var newVal = parseFloat(oldValue) + 1;
+        newVal = parseFloat(oldValue) + 1;
+
+        // if (!window.localStorage.getItem(id)) {
+        //   removeItemFromCart($button)
+        //   renderCartItem($button, newVal);
+        // } else {
+        //   //increment
+        // }
 
         incrementItemLocalStorage($button, newVal);
+        $button.parent().find("input").attr('value', newVal);
 
-        // const markup = `
-        // <p>${quantity}</p>
-        // <h3>${name}</h3>
-        // <p>$${Number(price).toFixed(2)}</p>
-        // <span><img src="/images/x-mark.png" alt="Remove from order" title="Remove from order"></span>
-        // `;
-
-        // $('#order-container').prepend($('<article>').addClass('container').html(markup));
-
-
-
+        removeItemFromCart($button);
+        renderCartItem($button, newVal);
 
       } else {
         // Don't allow decrementing below zero
         if (oldValue > 0) {
-          var newVal = parseFloat(oldValue) - 1;
+          newVal = parseFloat(oldValue) - 1;
           incrementItemLocalStorage($button, newVal);
+
+          removeItemFromCart($button);
+          renderCartItem($button, newVal);
 
           if (newVal === 0) {
             removeItemFromLocalStorage($button);
+            removeItemFromCart($button);
           }
 
         } else {
@@ -82,22 +164,26 @@ $(document).ready(function() {
 
     // Implement the onclick for the cross mark button
     $(".reset").on("click", function () {
-      var $button = $(this);
+      const $button = $(this);
 
       // set everythign to zero
       $button.parent().find("input").val("0");
       removeItemFromLocalStorage($button);
+
+      // remove item from cart
+      removeItemFromCart($button);
 
       // reset css to zero state
       $(this).siblings('input').css({ "color": "black" });
       $(this).siblings('input').css({ "font-weight": "normal" });
       $(this).css({ "visibility": "hidden" });
     });
+
+
   });
 
   // Open close side bar cart //
-  var cartOpen = false;
-  var numberOfProducts = 0;
+  let cartOpen = false;
 
   $('body').on('click', '.toggle-cart', toggleCart);
 
@@ -108,6 +194,7 @@ $(document).ready(function() {
       return;
     }
     openCart();
+    renderCartTotal();
   };
 
   function openCart() {
