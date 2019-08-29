9581b53431fcf5a42f80873023d52aa4f220f24c
Kevin Suen <kevinsuen11@gmail.com>
2019-08-28 21:08:31 -0400

    Update seeds

--- db/seeds/01_seed_users.sql
+++ db/seeds/01_seed_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
 INSERT INTO users (id, name, email, password, phone_number)
-VALUES (0, 'Kevin', 'kyoo95@mail.com', '123', '123123123');
+VALUES (0, 'Kevin', 'kyoo95@mail.com', '123', '123123123'),
+(1, 'Test', 'test@test.com', 'test', '1231231234');
