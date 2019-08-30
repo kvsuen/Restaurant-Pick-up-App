57f7b1084c82cea0278f7fbb9ac4d8c93c77fc8d
Kevin <kevinyoo0203@yahoo.com>
2019-08-30 03:09:29 +0000

    fix seed data causing register bug

--- db/seeds/01_seed_users.sql
+++ db/seeds/01_seed_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (id, name, email, password, phone_number)
-VALUES (0, 'Kevin', 'kyoo95@mail.com', '123', '123123123'),
-(1, 'Test', 'test@test.com', 'test', '1231231234');
+INSERT INTO users (name, email, password, phone_number)
+VALUES ('Kevin', 'kyoo95@mail.com', '123', '123123123'),
+('Test', 'test@test.com', 'test', '1231231234');
